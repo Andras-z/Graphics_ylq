da9c145abc0fa87d2ca5f6e2c12964fdea5036a8
Andras <andras0322@qq.com>
2022-06-28 22:32:46 +0800

    Home work of Graphics

---
+++ Hw0/Code/main.cpp
@@ -0,0 +1,64 @@
+#include<cmath>
+#include<eigen3/Eigen/Core>
+#include<eigen3/Eigen/Dense>
+#include<iostream>
+
+int main() {
+
+    // Basic Example of cpp
+    std::cout << "Example of cpp \n";
+    float a = 1.0, b = 2.0;
+    std::cout << a << std::endl;
+    std::cout << a/b << std::endl;
+    std::cout << std::sqrt(b) << std::endl;
+    std::cout << std::acos(-1) << std::endl;
+    std::cout << std::sin(30.0/180.0*acos(-1)) << std::endl;
+
+    // Example of vector
+    std::cout << "Example of vector \n";
+    // vector definition
+    Eigen::Vector3f v(1.0f,2.0f,3.0f);
+    Eigen::Vector3f w(1.0f,0.0f,0.0f);
+    // vector output
+    std::cout << "Example of output \n";
+    std::cout << v << std::endl;
+    // vector add
+    std::cout << "Example of add \n";
+    std::cout << v + w << std::endl;
+    // vector scalar multiply
+    std::cout << "Example of scalar multiply \n";
+    std::cout << v * 3.0f << std::endl;
+    std::cout << 2.0f * v << std::endl;
+
+    // Example of matrix
+    std::cout << "Example of matrix \n";
+    // matrix definition
+    Eigen::Matrix3f i,j;
+    i << 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0;
+    j << 2.0, 3.0, 1.0, 4.0, 6.0, 5.0, 9.0, 7.0, 8.0;
+    // matrix output
+    std::cout << "Example of output \n";
+    std::cout << "i = \n" << i << std::endl;
+    std::cout << "j = \n" << j << std::endl;
+    // matrix add i + j
+    std::cout << "i + j = \n" << i + j << std::endl;
+    // matrix scalar multiply i * 2.0
+    std::cout << "i * 2.0 = \n" << i * 2.0 << std::endl;
+    // matrix multiply i * j
+    std::cout << "i * j = \n" << i * j << std::endl;
+    // matrix multiply vector i * v
+    std::cout << "i * v = \n" << i * v << std::endl;
+
+    /**
+     * 给定一个点 P=(2,1), 将该点绕原点先逆时针旋转 45◦，再平移 (1,2), 计算出
+     * 变换后点的坐标（要求用齐次坐标进行计算）。
+     */
+    // Define point P
+    Eigen::Vector3f pointP(2.0f,1.0f,1.0f);
+    // Define rotation matrix M
+    Eigen::Matrix3f TransformMatrix;
+    TransformMatrix << std::cos(45.0), -std::sin(45.0), 1.0, std::sin(45.0), std::cos(45.0), 2.0, 0, 0, 1.0;
+    // M * P
+    std::cout << "M * P = \n" << TransformMatrix * pointP << std::endl;
+    return 0;
+}
